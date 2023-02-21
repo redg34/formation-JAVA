aef16f15353cf5f7c07af4fa341a88654d96ce4c
redg <regis.deroo@hotmail.com>
2023-02-21 11:01:13 +0100

    Run course from main

--- src/main/java/fr/aelion/Main.java
+++ src/main/java/fr/aelion/Main.java
@@ -2,16 +2,21 @@ package fr.aelion;
 
 import fr.aelion.models.Person;
 import fr.aelion.models.Student;
+import fr.aelion.run.CourseRun;
 import fr.aelion.run.PersonRun;
 import fr.aelion.run.StudentRun;
 
 public class Main {
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         PersonRun personRun = new PersonRun();
         personRun.run();
 
        new StudentRun().run();
     }
-
+*/
+    public  static  void  main(String[] args) {
+        CourseRun courseRun = new CourseRun();
+        courseRun.run();
+    }
 }
